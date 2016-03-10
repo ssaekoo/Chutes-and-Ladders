a4cb67235353e05da68e45b52a9792ed9e59e172
Stephen Saekoo <ssaekoo@gmail.com>
2016-03-10 00:26:31 -0800

    add reset function

--- javascript/reset.js
+++ javascript/reset.js
@@ -1,10 +1,14 @@
 resetGame = function() {
   new ChutesAndLadders;
-  
-  document.getElementsByClassName("player1-piece").className = "player-piece player1-piece hidden";
-  document.getElementsByClassName("player2-piece").className = "player-piece player2-piece hidden";
-  document.getElementsByClassName("player3-piece").className = "player-piece player3-piece hidden";
-  document.getElementsByClassName("player4-piece").className = "player-piece player4-piece hidden";
+  for (var i = 1; i <= 100; i++){
+    document.getElementById("board" + i + "-player1").className = "player-piece player1-piece hidden";
+    document.getElementById("board" + i + "-player2").className = "player-piece player2-piece hidden";
+    document.getElementById("board" + i + "-player3").className = "player-piece player3-piece hidden";
+    document.getElementById("board" + i + "-player4").className = "player-piece player4-piece hidden";
+  }
+  document.querySelectorAll(".player2-piece").className = "player-piece player2-piece hidden";
+  document.querySelectorAll(".player3-piece").className = "player-piece player3-piece hidden";
+  document.querySelectorAll(".player4-piece").className = "player-piece player4-piece hidden";
   document.getElementById("board0-player1").className = "player player-piece player1-piece";
   document.getElementById("board0-player2").className = "player player-piece player2-piece";
   document.getElementById("board0-player3").className = "player player-piece player3-piece";
